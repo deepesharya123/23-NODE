477ef1d6947d40ef8eb5dde67ab0310ec718df3a
Deepesh Arya <deepesharya@gmail.com>
2020-04-09 23:52:14 +0530

    AS

--- all files after23/36app.js
+++
@@ -1,8 +0,0 @@
-const request = require('request');
-const geocode = require('./utils/geocode')
-geocode('Kanpur', (error,data) =>{
-    console.log("Error ",error),
-    console.log("data ",data)
-    console.log("data ",data.location)
-    
-})
